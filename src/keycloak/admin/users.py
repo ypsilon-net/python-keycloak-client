245e1c73d2410809f074c4aa6eb46506fd2788a1
Sven <native2k@gmail.com>
2019-01-09 14:52:36 +0100

    more magic

--- src/keycloak/admin/users.py
+++ src/keycloak/admin/users.py
@@ -1,8 +1,60 @@
 import json
 from collections import OrderedDict
-from keycloak.admin import KeycloakAdminBase, KeycloakAdminCollection
+from keycloak.admin import KeycloakAdminBase, KeycloakAdminCollection, KeycloakAdminBaseElement
 
 __all__ = ('User', 'Users',)
+
+
+class User(KeycloakAdminBaseElement):
+    _id = None
+    _realm_name = None
+    _paths = {
+        'single': '/auth/admin/realms/{realm_name}/users/{id}',
+    }
+    _idents = {'email': 'email', 'first_name': 'firstName', 'last_name': 'lastName', 'enabled': 'enabled'}
+
+    def __init__(self, realm_name, id, *args, **kwargs):
+        self._id = id
+        self._realm_name = realm_name
+        super(User, self).__init__(*args, **kwargs)
+
+    @property
+    def first_name(self):
+        return self().get('firstName')
+
+    @property
+    def last_name(self):
+        return self().get('lastName')
+
+    @property
+    def email(self):
+        return self().get('email')
+
+    @property
+    def enabled(self):
+        return self().get('enabled')
+
+
+    def get(self):
+        res = self()
+        # res = self._admin.get(
+        #     url=self._admin.get_full_url(
+        #         self.get_path('single', realm_name=self._realm_name, id=self._id)
+        #     )
+        # )
+
+        if res: # format keys
+            if 'firstName' in res:
+                res['first_name'] = res.pop('firstName')
+            if 'lastName' in res:
+                res['last_name'] = res.pop('lastName')
+
+        return res
+
+    @property
+    def role_mappings(self):
+        from keycloak.admin.role_mappings import RoleMappings
+        return RoleMappings(admin=self._admin, realm_name=self._realm_name, user_id=self._id)
 
 
 class Users(KeycloakAdminBase, KeycloakAdminCollection):
@@ -10,10 +62,11 @@ class Users(KeycloakAdminBase, KeycloakAdminCollection):
         'max': -1, # turns off default max (100)
     }
     _paths = {
-        'collection': '/auth/admin/realms/{realm}/users',
-        'count': '/auth/admin/realms/{realm}/users/count',
+        'collection': '/auth/admin/realms/{realm_name}/users',
+        'count': '/auth/admin/realms/{realm_name}/users/count',
     }
     _realm_name = None
+    _itemclass = User
 
     def __init__(self, realm_name, *args, **kwargs):
         self._realm_name = realm_name
@@ -29,10 +82,9 @@ class Users(KeycloakAdminBase, KeycloakAdminCollection):
 
     def count(self):
         return self._admin.get(
-            self._admin.get_full_url(
-                self.get_path('count', realm=self._realm_name)
-            )
+            self._admin.get_full_url(realm_name=self._realm_name)
         )
+
 
     def create(self, username, **kwargs):
         """
@@ -70,37 +122,10 @@ class Users(KeycloakAdminBase, KeycloakAdminCollection):
         )
 
     def _url_collection_params(self):
-        return {'realm': self._realm_name}
+        return {'realm_name': self._realm_name}
 
-
-class User(KeycloakAdminBase):
-    _id = None
-    _realm_name = None
-    _paths = {
-        'single': '/auth/admin/realms/{realm}/users/{id}',
-    }
-
-    def __init__(self, realm_name, id, *args, **kwargs):
-        self._id = id
-        self._realm_name = realm_name
-        super(User, self).__init__(*args, **kwargs)
-
-    def get(self):
-        res = self._admin.get(
-            url=self._admin.get_full_url(
-                self.get_path('single', realm=self._realm_name, id=self._id)
-            )
+    def _url_item_params(self, data):
+        return dict(
+            id=data['id'], admin=self._admin, realm_name=self._realm_name
         )
 
-        if res: # format keys
-            if 'firstName' in res:
-                res['first_name'] = res.pop('firstName')
-            if 'lastName' in res:
-                res['last_name'] = res.pop('lastName')
-
-        return res
-
-    @property
-    def role_mappings(self):
-        from keycloak.admin.role_mappings import RoleMappings
-        return RoleMappings(admin=self._admin, realm_name=self._realm_name, user_id=self._id)
