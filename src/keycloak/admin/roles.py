a0c60bfed9d882d55e3f00901a2a0a6e8ffb6885
Sven <native2k@gmail.com>
2019-01-26 15:51:00 +0100

    fix roles

--- src/keycloak/admin/roles.py
+++ src/keycloak/admin/roles.py
@@ -55,7 +55,7 @@ class Role(KeycloakAdminBaseElement):
         'id' : 'id',
         'name': 'name',
         'description': 'description',
-        'composite': 'composite',
+        #'composite': 'composite',
         'container_id': 'containerId',
         'client_role': 'clientRole',
     }
